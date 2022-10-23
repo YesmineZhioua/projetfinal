4a13ee9ec3803d0fe3beddf197412b4d5fa91fe9
kaoutherzhioua <86618217+kaoutherzhioua@users.noreply.github.com>
2022-10-23 13:01:08 +0100

    hj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import MovieCard from "./components/movie-card";
+
 
 function App() {
+
+  const API_URL = "https://api.themoviedb.org/3"
+  const BACKGROUND_IMAGE_PATH = "https://image.tmdb.org/t/p/w1280/"
+  const API_KEY = "502709b57a68d03a1d751fc801b2b4ea"
+  const [movies, setMovies] = useState([])
+  const [selectedMovie, setSelectedMovie] = useState({})
+  const [searchKey, setSearchKey] = useState("")
+
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover"
+    const {data:{results}} = await axios.get(`${API_URL}/${type}/movie`,{
+      params: {
+        api_key: API_KEY,
+        query: searchKey
+      }
+    })
+    setMovies(results)
+  }
+
+  useEffect(() =>{
+    fetchMovies(searchKey)
+  })
+
+
+  const renderMovies = () => (
+    movies.map(movie => (
+      <MovieCard
+      key={movie.id}
+      movie={movie}
+      selectMovie={setSelectedMovie}
+      />
+    ))
+  )
+ 
+  const searchMovies = (e) =>{
+    e.preventDefault()
+    fetchMovies()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={"info"}  style={{backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)) ,url(${BACKGROUND_IMAGE_PATH}${selectedMovie.backdrop_path})`}}>
+        <header>
+          <p className={"title"}>MyMovies</p>
+          <form onSubmit={searchMovies}>
+            <input type={"text"} onChange={(e) => setSearchKey(e.target.value)} />
+
+          </form>
+        </header>
+        <div className={"info-content"}>
+          <h1>{selectedMovie.title}</h1>
+          {selectedMovie.overview ? <p>{selectedMovie.overview}</p> : null}
+        </div>
+      </div>
+      <div className="container">
+        {renderMovies()}
+      </div>
     </div>
-  );
+    
+  )
 }
 
-export default App;
+export default App
